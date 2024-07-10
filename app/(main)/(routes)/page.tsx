121d3b5d82f69165a6c5dcfff0fb8cd91a85d226
Vivek Singh <vivek.singh@codingninjas.com>
2024-07-11 03:45:24 +0530

    added light-dark theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,13 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-       This is a protected route
+    <div className="flex flex-col">
+       <UserButton afterSignOutUrl="/"></UserButton>
+       <ModeToggle></ModeToggle>
     </div>
   );
 }
