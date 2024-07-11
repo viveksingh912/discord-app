4b25d17b08c2fb1e8d859abe46e8e6e3971cb39b
Vivek Singh <vivek.singh@codingninjas.com>
2024-07-12 03:35:21 +0530

    added db .ts and initial profile

---
+++ lib/initial-profile.ts
@@ -0,0 +1,24 @@
+import { currentUser, redirectToSignIn } from "@clerk/nextjs/server";
+import { db } from "./db";
+
+export const initialProfile=async()=>{
+    const user= await currentUser();
+    if(!user){
+        return redirectToSignIn();
+    }
+    const profile = await db.profile.findUnique({where:{
+        usedId: user.id
+    }});
+    if(profile){
+    return profile;
+    }
+    const newProfile = await db.profile.create({
+        data:{
+            usedId: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            imageUrl: user.imageUrl,
+            email: user.emailAddresses[0].emailAddress
+        }
+    })
+    return newProfile;
+}
