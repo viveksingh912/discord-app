b7ab2b84a8196abb81b1d2eebecf03d01d986e00
Vivek Singh <vivek.singh@codingninjas.com>
2024-07-15 01:26:52 +0530

    added create channel

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -68,6 +68,7 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {isModerator && (
             <DropdownMenuItem
+            onClick={()=>onOpen("createChannel")}
             className=" px-3 py-2 text-sm cursor-pointer"
             >
                 Create Channel
--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -2,7 +2,7 @@ import { Server } from "@prisma/client";
 import {create} from "zustand";
 
 export type ModalType = "createServer" | "invite" | "editServer"
-| "members";
+| "members" | "createChannel";
 
 interface ModalData{
     server?: Server
