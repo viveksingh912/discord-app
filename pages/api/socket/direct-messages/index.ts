12183be784c6dabc01f68e9656457f2e62fd6d5b
Vivek Singh <vivek.singh@codingninjas.com>
2024-07-19 15:20:36 +0530

    added send edit delete message for personal chats

---
+++ pages/api/socket/direct-messages/index.ts
@@ -0,0 +1,93 @@
+import { currentProfilePages } from "@/lib/currentProfile-pages";
+import { db } from "@/lib/db";
+import { NextApiResponseServerIo } from "@/types";
+import { error } from "console";
+import { NextApiRequest } from "next";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponseServerIo,
+) {
+    if(req.method !=="POST"){
+        return res.status(405).json({error: "method not allowed"});
+    }
+    try{
+        const profile = await currentProfilePages(req);
+        const { content, fileUrl}=req.body;
+        const {conversationId} =req.query;
+        if(!profile){
+            return res.status(401).json({error: "Unauthorised"});
+        }
+        if(!conversationId){
+            return res.status(401).json({error: "conversation id missing"});
+        }
+    
+        if(!content){
+            return res.status(401).json({error: "Content id missing"});
+        }
+
+        
+        const conversation= await db.conversation.findFirst({
+            where:{
+                id: conversationId as string,
+                OR:[
+                    {
+                        memberOne:{
+                            profileId: profile.id
+                        }
+                    },
+                    {
+                        memberTwo:{
+                            profileId: profile.id
+                        }
+                    }
+                ]
+            },
+            include:{
+                memberOne:{
+                    include:{
+                        profile:true,
+                    }
+                },
+                memberTwo:{
+                    include:{
+                        profile:true,
+                    }
+                }
+            }
+        })
+
+        if(!conversation){
+            return res.status(404).json({message: "conversatioin not found"});
+        }
+        
+        const member= conversation.memberOne.profileId===profile.id?conversation.memberOne:conversation.memberTwo;
+
+        if(!member){
+            return res.status(404).json({message: "Member not found"});
+        }
+
+        const message = await db.directMessage.create({
+            data:{
+                content,
+                fileUrl,
+                conversationId: conversationId as string,
+                memberId: member.id,
+            },
+            include:{
+                member:{
+                    include:{
+                        profile: true,
+                    }
+                }
+            }
+        });
+
+        const channelKey = `chat:${conversationId}:messages`;
+        res?.socket?.server?.io?.emit(channelKey,message);
+        return res.status(200).json(message);
+}catch(error){
+    console.log("DIRECT_MESSAGES_POSt", error);
+    return res.status(500).json({message: "Internal Error"});
+}
+}
